284c782a9e60aa1c122bccb292d5ef97906da3fd
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2026-01-13 23:56:29 +0600

    sendEmail

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,29 +1,33 @@
 const userModel = require("../models/user.model");
 const { apiResponse } = require("../utils/apiResponse");
 const bcrypt = require('bcrypt');
+const asyncHandler = require("../utils/asyncHandler");
+const sendEmail = require("../helpers/sendEmail");
 
 
-exports.signupController = async (req, res) => {
-  
-
+exports.signupController = asyncHandler(
+    async (req, res) => {
     let { fullname, email, password, phone, address, photo } = req.body;
       bcrypt.hash(password, 12,async function(err, hash) {
        if(err){
         apiResponse(res,500,err)
        }
+        let user = new userModel({
+        fullname, email, password:hash, phone, address, photo
+    })
+    await user.save()
+    sendEmail(email,fullname)
+    apiResponse(res,201,"user created successfull",user)
        
     });
-    let user = new userModel({
-        fullname, email, password, phone, address, photo
-    })
-
-    await user.save()
-    apiResponse(res,201,"user created successfull",user)
-
-
-
 }
-exports.productsController = (req, res) => {
-    res.send("Products Controller");
-}
+)
+
+
+
+
+
+// exports.productsController = (req, res) => {
+//     res.send("Products Controller");
+// }
 
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -28,6 +28,10 @@ const userSchema = new mongoose.Schema({
     },
     photo:{
         type: String,
+    },
+    otp:{
+        type:String,
+        
     }
 }, 
 {
