7bf0a704dc4679512b796a8687c654a46eb539b9
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2026-01-14 11:08:43 +0600

    userlogin

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,25 +3,50 @@ const { apiResponse } = require("../utils/apiResponse");
 const bcrypt = require('bcrypt');
 const asyncHandler = require("../utils/asyncHandler");
 const sendEmail = require("../helpers/sendEmail");
+const otpNumber = require("../helpers/otp");
 
 
 exports.signupController = asyncHandler(
     async (req, res) => {
-    let { fullname, email, password, phone, address, photo } = req.body;
+    let { fullname, email, password, phone, address, photo,otp } = req.body;
       bcrypt.hash(password, 12,async function(err, hash) {
        if(err){
         apiResponse(res,500,err)
        }
+       let otp = otpNumber()
         let user = new userModel({
-        fullname, email, password:hash, phone, address, photo
+        fullname, email, password:hash, phone, address, photo,otp
     })
     await user.save()
-    sendEmail(email,fullname)
+    sendEmail(email,fullname,otp)
     apiResponse(res,201,"user created successfull",user)
        
     });
 }
 )
+
+
+exports.loginController = asyncHandler(async(req,res)=>{
+    let {email, password}= req.body
+  let loginUser = await userModel.findOne({email})
+  if(!loginUser){
+    apiResponse(res,404,"email not found")
+  }else{
+    bcrypt.compare(password, loginUser.password,function(err,result){
+        if(err){
+            apiResponse(res,500,"some went wrong")
+        }else{
+            if(!result){
+              apiResponse(res,401,"Invalid password")
+            }else{
+
+              apiResponse(res,200,"user login success",loginUser)
+            }
+            
+        }
+    })
+  }
+})
 
 
 
--- server/route/api/auth/index.js
+++ server/route/api/auth/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { signupController } = require('../../../controllers/auth.controller');
+const { signupController, loginController } = require('../../../controllers/auth.controller');
 const router = express.Router();
 
 
 router.post("/signup", signupController);
+router.post("/login", loginController)
 
 // router.post("/login" , (req, res) => {
 //     res.send("User Login Route");
