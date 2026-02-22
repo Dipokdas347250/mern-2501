523b6c842e00b99e7d0548b3b075431ee626e9dc
Dipokdas437250 <dipakdas24680@gmail.com>
2026-02-22 23:01:54 +0600

    Main Product created successfully

--- server/controllers/mainProduct.controller.js
+++ server/controllers/mainProduct.controller.js
@@ -1,15 +1,16 @@
 const productModel = require("../models/product.model");
+const { apiResponse } = require("../utils/apiResponse");
 const asyncHandler = require("../utils/asyncHandler");
 
 exports.allMainProductController = asyncHandler(async (req, res) => {
-    console.log(req.files);
-    
-  res.send(req.body);
-
+  let filenames = req.files.map((file) => {
+  return `${process.env.SERVER_URL}/${file.filename }`     
+  });
   let products = new productModel({
     ...req.body,
-    images: req.files.map(file => file.path)
+    image: filenames[0]
   })
   await products.save();
+  apiResponse(res, 201, "Main Product created successfully", products);
   
 })
