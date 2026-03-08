9d62928a85e4a50f76e0c75b9d3088bbd545acba
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2026-03-08 16:13:32 +0600

    add to cart done

--- server/controllers/addcart.controller.js
+++ server/controllers/addcart.controller.js
@@ -6,32 +6,61 @@ const asyncHandler = require("../utils/asyncHandler");
 exports.addCartController = asyncHandler(async(req ,res)=>{
     
     let {variant,user,quntity,product}= req.body;
-    let productData = await productModel.findOne({_id:product})
+    let cartData = await cartModel.findOne({product}).populate({
+      path: "product",
+      selete: "price"
+    })
 
-   if(productData.variantType == "multivariant"){ 
-      if(!variant){
-         apiResponse(res, 404,"variant is required...")
+    if(cartData){
+      cartData.quntity++;
+      cartData.totalprice = cartData.product.price * cartData.quntity;
+      await cartData.save()
+      apiResponse(res ,200, "quntity updated")
 
+    }else{
+
+       let productData = await productModel.findOne({_id:product})
+       let totalprice = productData.price * (quntity ? quntity: 1);
+   
+   
+   
+      if(productData.variantType == "multivariant"){ 
+         if(!variant){
+            apiResponse(res, 404,"variant is required...")
+   
+         }else{
+   
+            let addtocart = new cartModel({
+                user,
+                quntity,
+                product,
+                variant,
+                totalprice
+             });
+              await addtocart.save()
+             apiResponse(res, 201,"product add to cart...", addtocart)
+         }
+          
       }else{
-
-         let addtocart = new cartModel({
-             user,
-             quntity,
-             product,
-             variant
-          });
-           await addtocart.save()
-          apiResponse(res, 201,"product add to cart...", addtocart)
+       // single variant product
+        let addtocart = new cartModel({
+           user,
+           quntity,
+           product,
+           totalprice
+        });
+        await addtocart.save()
+        apiResponse(res, 201,"product add to cart...", addtocart)
       }
-       
-   }else{
-    // single variant product
-     let addtocart = new cartModel({
-        user,
-        quntity,
-        product,
-     });
-     await addtocart.save()
-     apiResponse(res, 201,"product add to cart...", addtocart)
-   }
-} )
+    }
+
+})
+
+exports.singleCartController = asyncHandler(async(req, res)=>{
+   let {user} = req.params;
+   let getCartlist = await cartModel.find({user}).populate({
+      path:"product",
+      select: "title price image"
+   }).select("-user -updatedAt -createdAt")
+   apiResponse(res, 200 , "single cart fatch ...",getCartlist)
+})
--- server/route/api/cart/index.js
+++ server/route/api/cart/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { addCartController } = require('../../../controllers/addcart.controller');
+const { addCartController, singleCartController } = require('../../../controllers/addcart.controller');
 const router = express.Router();
 
 
 router.post("/add-cart", addCartController)
+router.get("/singlecart/:user" , singleCartController)
 
 
 
