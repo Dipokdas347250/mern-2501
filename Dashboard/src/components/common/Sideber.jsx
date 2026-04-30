b3ee498b07bf024a1f6cb3bd69ba5ed5b246d2e4
Dipokdas347250 <dipakdas24680@gmail.com>
2026-04-30 23:35:47 +0600

    demo

--- Dashboard/src/components/common/Sideber.jsx
+++ Dashboard/src/components/common/Sideber.jsx
@@ -93,6 +93,7 @@ const Sideber = () => {
                 <div className="flex items-center gap-3 p-2 cursor-pointer duration-300 ease-in-out hover:bg-green-300/20 rounded-r-lg border-l-4 border-[#064e3b] hover:border-green-300">
                   <GiVerticalBanner className='text-[16px] text-white' />
                   <h2 className=' text-white text-lg font-semibold'>Add Banner</h2>
+                  <h2 className=' text-white text-lg font-semibold'>Add Banner</h2>
                 </div>
               </Link>
             </div>
